3a875364f0ffc11e81f6e75a46497f625fb0c359
Franciscaa77 <kikapalma004@gmail.com>
2024-06-25 23:23:54 +0100

    ooo_versions

--- src/views/admin/adminversions.js
+++ src/views/admin/adminversions.js
@@ -1,30 +1,35 @@
 import React from 'react';
+import Breadcrumbs from "../../components/breadcrumbs";
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import './adminversions.css';
 
 export default function adminversions() {
-  return (
-    <div className="container d-flex flex-column align-items-center justify-content-center" style={{ height: '100vh' }}>
+    return (
+        <div className="container d-flex flex-column align-items-center justify-content-center" style={{ height: '100vh' }}>
             <Helmet>
-                <title>Version - LogicLeap</title>
+                <title>Versions - LogicLeap</title>
             </Helmet>
+            <Breadcrumbs page1="Products" page2="Product Name" page3="Versions" link1="/" link2="/"></Breadcrumbs>
             <div className="col-md-6 bg-white p-4 rounded-3 form-container">
-                <div className="breadcrumb">
-                    <a href="/">Products</a> / <a href="/">Product Name</a> / Versions
-                </div>
                 <form>
-                    <div className="form-group mt-3">
-                        <label htmlFor="version">Version</label>
-                        <input type="text" className="form-control" id="version" name="version" placeholder="1.0.3b" />
-                    </div>
-                    <div className="form-group mt-3">
-                        <label htmlFor="status">Status</label>
-                        <select className="form-control" id="status" name="status">
-                            <option>Alpha</option>
-                            <option>Beta</option>
-                            <option>Stable</option>
-                        </select>
+                    <div className="row g-3">
+                        <div className="col-sm-6">
+                            <label htmlFor="version" className="form-label">
+                                Version
+                            </label>
+                            <input type="text" className="form-control" id="version" placeholder="1.0.3b" />
+                        </div>
+                        <div className="col-sm-6">
+                            <label htmlFor="status" className="form-label">
+                                Status
+                            </label>
+                            <select className="form-control" id="status" name="status">
+                                <option>Alpha</option>
+                                <option>Beta</option>
+                                <option>Stable</option>
+                            </select>
+                        </div>
                     </div>
                     <div className="form-group mt-3">
                         <label htmlFor="download">Download</label>
@@ -38,6 +43,6 @@ export default function adminversions() {
                 </form>
             </div>
         </div>
-  );
+    );
 };
 
