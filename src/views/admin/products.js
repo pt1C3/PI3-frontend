01d17fe4b395c1e7b6dd71847f2a8ff7963ef0f1
raso04 <pv26511@alunos.estgv.ipv.pt>
2024-06-26 17:03:56 +0100

    product list

--- src/views/admin/products.js
+++ src/views/admin/products.js
@@ -1,6 +1,6 @@
 
-import BillingTable from "../components/billing-table.js";
-import "./login.css";
+import BillingTable from "../../components/billing-table-products.js";
+import "./products.css";
 import { Helmet } from 'react-helmet';
 
 export default function Products() {
@@ -10,7 +10,7 @@ export default function Products() {
       <Helmet>
         <title>Payment history - CreatiVortex</title>
       </Helmet>
-      <BillingTable title="Product Name" type="Software" status="Pending" maxUsers="-" billingDate="2023-03-12" billingAmount="€19.99" paymentDate="2023-03-12 11:45 AM" startDate="2023-03-12" action="Pay" />
+      <BillingTable title="Product Name" status="Pending" versions="1.0.1" billingDate="2023-03-12" billingAmount="€19.99" paymentDate="2023-03-12 11:45 AM" startDate="2023-03-12" action="Pay" />
     </div>
   );
 }
