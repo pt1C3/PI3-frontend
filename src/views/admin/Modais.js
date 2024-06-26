2ea13c53ca4eccf32aeda1cf841ade16c65528bd
lRenasui <107072037+lRenasui@users.noreply.github.com>
2024-06-26 20:05:52 +0100

    mudando

--- src/views/admin/Modais.js
+++ src/views/admin/Modais.js
@@ -1,64 +1,78 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Modais.css';
 
 export default function App() {
   return (
-    <div className="modal-container">
-      <h1>Testando</h1>
-      <div className="modal-content">
-        <div className="image-upload-section box">
-          <div className="image-placeholder">
-            <img src="" alt="Placeholder" />
+    <div className="container wrapper mt-4">
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card">
+            <div className="card-body">
+              <div className="form-group">
+                <label htmlFor="productImage">Image</label>
+                <div className="mb-3">
+                  <div className="image-placeholder"></div>
+                  <input type="file" className="form-control-file mt-2" id="productImage" />
+                </div>
+              </div>
+              <button className="btn btn-primary btn-block">Save all</button>
+            </div>
           </div>
-          <button className="upload-button">Upload</button>
-          <button className="delete-button">Delete</button>
         </div>
-        
-        <div className="general-information box">
-          <h2>General Information</h2>
-          <label>
-            Name
-            <input type="text" name="name" placeholder="Product" />
-          </label>
-          <label>
-            Category
-            <select name="category">
-              <option value="productivity">Productivity</option>
-              {/* Add more options as needed */}
-            </select>
-          </label>
-          <label>
-            Description
-            <textarea name="description" placeholder="Description"></textarea>
-          </label>
-        </div>
-        
-        <div className="requirements box">
-          <h2>Requirements</h2>
-          <label>
-            Operating System
-            <input type="text" name="os" placeholder="Windows 10 64 bits" />
-          </label>
-          <label>
-            Processor
-            <input type="text" name="processor" placeholder="Multicore Intel" />
-          </label>
-          <label>
-            RAM
-            <input type="text" name="ram" placeholder="8 GB" />
-          </label>
-          <label>
-            Hard Disk Space
-            <input type="text" name="hdd" placeholder="" />
-          </label>
-          <label>
-            Graphics Card
-            <input type="text" name="gpu" placeholder="" />
-          </label>
-          <label>
-            <input type="checkbox" name="internet-required" defaultChecked />
-            This product needs internet connectivity to activate the software
-          </label>
+        <div className="col-md-8">
+          <div className="card mb-3">
+            <div className="card-body">
+              <h5 className="card-title">General Information</h5>
+              <div className="form-group">
+                <label htmlFor="productName">Name</label>
+                <input type="text" className="form-control" id="productName" placeholder="Product" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="productCategory">Category</label>
+                <select className="form-control" id="productCategory">
+                  <option>Productivity</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="productDescription">Description</label>
+                <textarea className="form-control" id="productDescription" rows="3"></textarea>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Requirements</h5>
+              <div className="form-group">
+                <label htmlFor="os">Operating System</label>
+                <input type="text" className="form-control" id="os" placeholder="Windows 10 64 bits" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="processor">Processor</label>
+                <input type="text" className="form-control" id="processor" placeholder="Multicore Intel" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="ram">RAM</label>
+                <select className="form-control" id="ram">
+                  <option>8 GB</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="hdd">Hard Disk Space</label>
+                <input type="text" className="form-control" id="hdd" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="graphicsCard">Graphics Card</label>
+                <input type="text" className="form-control" id="graphicsCard" />
+              </div>
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="internetRequirement" />
+                <label className="form-check-label" htmlFor="internetRequirement">
+                  This product needs internet connectivity to activate the software
+                </label>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
