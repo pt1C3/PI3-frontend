ede6950c5f57cdc67875a269537e9fa24ef2a7b0
Franciscaa77 <kikapalma004@gmail.com>
2024-06-27 11:37:50 +0100

    Update product.js

--- src/views/manager/product.js
+++ src/views/manager/product.js
@@ -4,10 +4,30 @@ import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import './product.css';
 
-export default function product() {
+export default function Product() {
     return (
-       <div>
-
-       </div>
+<body>
+    <div class="container mt-4">
+        <div class="card border-dashed mb-4">
+            <div class="card-body d-flex align-items-center">
+             <div className="mb-3">
+                      <div className="image-placeholder"></div>
+                      <label className="btn btn-secondary mt-2" htmlFor="productImage">Choose File</label>
+                      <input type="file" className="form-control-file mt-2" id="productImage" /> 
+                    </div>
+                </div>
+                <div>
+                    <h5 class="card-title mb-0">Creativortex</h5>
+                    <small class="text-muted">Design</small>
+                </div>
+            </div>
+        </div>
+        <div class="card border-dashed">
+            <div class="card-body">
+                <h5 class="card-title">Addons</h5>
+                <p class="card-text">No items to show.</p>
+            </div>
+        </div>
+</body >
     );
 };
