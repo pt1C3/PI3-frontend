adbd23a4bff94eb4c9c45bd405aff1a5ce3a842c
Franciscaa77 <kikapalma004@gmail.com>
2024-07-04 18:47:54 +0100

    Update billing-table-costumplan.js

--- src/components/billing-table-costumplan.js
+++ src/components/billing-table-costumplan.js
@@ -2,7 +2,7 @@ import { version } from 'react';
 import costumplan from '../views/admin/costumplan';
 import './billing-table-costumplan.css';
 
-const BillingTable = ({ title, type, billingDate, paymentDate, status, maxUsers, billingAmount, startDate, action }) => { 
+const BillingTable = ({ title, type, billingDate, paymentDate, status, maxUsers, billingAmount, startDate, action }) => {
 
     const data = [
         {
@@ -24,37 +24,39 @@ const BillingTable = ({ title, type, billingDate, paymentDate, status, maxUsers,
             <table className="table table-bordered">
                 <thead>
                     <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Business</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Versions</th>
-                    <th scope="col">Active Costumers</th>
-                    <th scope="col">billingAmount (yr.)</th>
-                    <th scope="col">billingAmount (mo.)</th>
-                    <th scope="col">Actions</th>
+                        <th scope="col">#</th>
+                        <th scope="col">Business</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Versions</th>
+                        <th scope="col">Active Costumers</th>
+                        <th scope="col">billingAmount (yr.)</th>
+                        <th scope="col">billingAmount (mo.)</th>
+                        <th scope="col">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((item, index) => (
                         <tr key={index}>
                             <td>{item.id}</td>
-                            <td>{title}
-                            </td>
+                            <td>{item.title}</td>
                             <td>
-                            <span className={`text-${business === 'António Mendes' ? status === 'Pending' ? 'warning' : status === 'Payed' ? 'success' : 'danger'}`}>
-                                    {status}
+                                <span className={`text-${item.status === 'Pending' ? 'warning' : item.status === 'Payed' ? 'success' : 'danger'}`}>
+                                    {item.status}
                                 </span>
                             </td>
-                            <td>{version}</td>
-                            <td>{billingDate}</td>
-                            <td>{billingAmount}</td>
-                            <td>{billingAmount}</td>
+                            <td>{item.version}</td>
+                            <td>{item.billingDate}</td>
+                            <td>{item.billingAmount}</td>
+                            <td>{item.billingAmount}</td>
+                            {item.business === 'António Mendes' && (
+                                <td>{item.business}</td>
+                            )}
                             <td>
-                                {action && <a className="linknormal" href="#">{action}</a>}
+                                {item.action && <a className="linknormal" href="#">{item.action}</a>}
                             </td>
-                            
                         </tr>
                     ))}
+
                 </tbody>
             </table>
         </div>
