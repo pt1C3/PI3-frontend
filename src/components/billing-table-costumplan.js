f992b0c074e4fa7c1368479f723ea41420ae1ca6
Franciscaa77 <kikapalma004@gmail.com>
2024-07-04 18:32:15 +0100

    AAAAAAAAAAAAAAAA_help

---
+++ src/components/billing-table-costumplan.js
@@ -0,0 +1,64 @@
+import { version } from 'react';
+import costumplan from '../views/admin/costumplan';
+import './billing-table-costumplan.css';
+
+const BillingTable = ({ title, type, billingDate, paymentDate, status, maxUsers, billingAmount, startDate, action }) => { 
+
+    const data = [
+        {
+            id: '1',
+        },
+        {
+            id: '2',
+        },
+        {
+            id: '3',
+        },
+        {
+            id: '4',
+        },
+    ];
+
+    return (
+        <div className="container mt-5 billing-table">
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Business</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Versions</th>
+                    <th scope="col">Active Costumers</th>
+                    <th scope="col">billingAmount (yr.)</th>
+                    <th scope="col">billingAmount (mo.)</th>
+                    <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.id}</td>
+                            <td>{title}
+                            </td>
+                            <td>
+                            <span className={`text-${business === 'António Mendes' ? status === 'Pending' ? 'warning' : status === 'Payed' ? 'success' : 'danger'}`}>
+                                    {status}
+                                </span>
+                            </td>
+                            <td>{version}</td>
+                            <td>{billingDate}</td>
+                            <td>{billingAmount}</td>
+                            <td>{billingAmount}</td>
+                            <td>
+                                {action && <a className="linknormal" href="#">{action}</a>}
+                            </td>
+                            
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default BillingTable;
