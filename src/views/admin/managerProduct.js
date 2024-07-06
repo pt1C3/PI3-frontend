3869ab520566c405632bb0b8a7716537429a1002
lRenasui <107072037+lRenasui@users.noreply.github.com>
2024-07-06 05:47:55 +0100

    manager product

--- src/views/admin/managerProduct.js
+++ src/views/admin/managerProduct.js
@@ -1,29 +1,84 @@
 import React from 'react';
+import "./managerProduct.css";
 
 export default function ManagerProduct() {
     return (
-<body>
-    <div class="container mt-4">
-        <div class="card border-dashed mb-4">
-            <div class="card-body d-flex align-items-center">
-             <div className="mb-3">
-                      <div className="image-placeholder"></div>
-                      <label className="btn btn-secondary mt-2" htmlFor="productImage">Choose File</label>
-                      <input type="file" className="form-control-file mt-2" id="productImage" /> 
+        <div className="wrapper d-flex flex-column justify-content-center align-items-center">
+        <div className="content mt-5">
+          <div className="card mb-3">
+            <div className="card-body">
+              <div className="custom-title">Creativortex</div>
+              <div className="divider"></div>
+              <div className="form-group">
+                <div className="logo">
+                  <div className="logo-box">
+                    <span className="logo-text-c">C</span>
+                    <span className="logo-text-v">v</span>
+                  </div>
+                </div>
+                <div className="title">
+                  <h1>Creativortex</h1>
+                  <p>Design</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card mb-3">
+            <div className="card-body">
+              <div className="custom-title">Addons</div>
+              <div className="divider"></div>
+              <div className="form-group">
+                <p>No items to show.</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-body">
+              <div className="custom-title">Manage Licenses</div>
+              <div className="divider"></div>
+              <div className="form-group licenses">
+                <div className="licenses-sidebar">
+                  <div className="search-bar">
+                    <input type="text" placeholder="Search" />
+                  </div>
+                  <div className="license-list">
+                    <div className="license-item">
+                      <p>António Mendes</p>
+                      <p>antoniomendes@empresa.pt</p>
+                      <span className="license-check">&#10003;</span>
                     </div>
+                    <div className="license-item">
+                      <p>Employee 1</p>
+                      <p>employee1@gmail.com</p>
+                      <span className="license-check">&#10003;</span>
+                    </div>
+                    <div className="license-item">
+                      <p>Employee 2</p>
+                      <p>employee2@gmail.com</p>
+                      <span className="license-check">&#10003;</span>
+                    </div>
+                    <div className="license-item">
+                      <p>Employee 3</p>
+                      <p>employee3@gmail.com</p>
+                      <span className="license-check">&#10003;</span>
+                    </div>
+                  </div>
+                  <div className="add-license">
+                    <button>+</button>
+                  </div>
                 </div>
-                <div>
-                    <h5 class="card-title mb-0">Creativortex</h5>
-                    <small class="text-muted">Design</small>
+                <div className="licenses-details">
+                  <p><strong>António Mendes</strong></p>
+                  <p>antoniomendes@empresa.pt</p>
+                  <p>License key</p>
+                  <p>12345 12345 54321 54321 123 (Designer Pack)</p>
+                  <button className="remove-license">Remove License</button>
+                  <p className="license-count">15/30</p>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-        <div class="card border-dashed">
-            <div class="card-body">
-                <h5 class="card-title">Addons</h5>
-                <p class="card-text">No items to show.</p>
-            </div>
-        </div>
-</body >
+      </div>
     );
 };
