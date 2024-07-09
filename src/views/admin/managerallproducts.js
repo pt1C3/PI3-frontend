e8f4618e9fe75d4d21c2a548a290f7f53c5f1c0c
raso04 <pv26511@alunos.estgv.ipv.pt>
2024-07-09 17:15:45 +0100

    managerAllProducts

--- src/views/admin/managerallproducts.js
+++ src/views/admin/managerallproducts.js
@@ -2,104 +2,24 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./managerallproducts.css";
 
-export default function Modais() {
+export default function managerallproducts() {
   return (
     <div className="wrapper d-flex flex-column justify-content-center mx-auto">
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
+      <div className="container mt-5">
+        <div className="row">
+          {[...Array(10)].map((_, index) => (
+            <div key={index} className="col-md-4 mb-2">
+              <div className="card">
+                <div className="card-body d-flex justify-content-between align-items-center">
+                  <div className={`image-placeholder ${index === 4 ? 'design-pack' : ''}`}></div>
+                  <span>{index === 4 ? 'Design Pack' : 'Product Name'}</span>
+                  <span>15/30</span>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Design Pack</span>
-              <span>15/30</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <div className="card-body d-flex justify-content-between align-items-center">
-              <div className="image-placeholder"></div>
-              <span>Product Name</span>
-              <span>15/30</span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
-  </div>
-
   );
 }
